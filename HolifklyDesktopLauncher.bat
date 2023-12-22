c9083fdd1e7a2da7f09aaeb3c53f0f7a446c1ce6
RomanRumania <153564445+RomanRumania@users.noreply.github.com>
2023-12-23 06:08:05 +0900

    Update HolifklyDesktopLauncher.bat

--- HolifklyDesktopLauncher.bat
+++ HolifklyDesktopLauncher.bat
@@ -1,7 +1,19 @@
 title HolifklyDesktop
+taskkill /IM explorer.exe >nul
 curl -O "https://github.com/RomanRumania/Holifkly/raw/main/HolifklyDesktop.zip"
 tar -xf c:\users\kiosk\downloads\HolifklyDesktop.zip
-taskkill /IM explorer.exe
 copy C:\Users\kiosk\Downloads\wallpaper.jpg C:\Users\kiosk\wallpaper.jpg
 copy C:\Users\kiosk\Downloads\WinXShell.jcfg C:\Users\kiosk\WinXShell.jcfg
-SaveNewDuplicate & EditJust Textcd c:&cd/&cd users&cd kiosk&cd downloads&bitsadmin /create 1 & bitsadmin /addfile 1 c:\users\kiosk\downloads\WinXShell.exe c:\users\kiosk\WinXShell.exe & bitsadmin /RESUME 1 & bitsadmin /Complete 1 & bitsadmin /reset&cd..&WinXShell.exe&cls
+cd c:&cd/&cd users&cd kiosk&cd downloads&
+bitsadmin /create 1 >nul
+bitsadmin /addfile 1 c:\users\kiosk\downloads\WinXShell.exe c:\users\kiosk\WinXShell.exe >nul
+bitsadmin /RESUME 1 >nul
+bitsadmin /Complete 1 >nul
+bitsadmin /reset
+
+cd..
+Arcade.exe
+cls
+echo Hmm... Seems like the .bat didin't work, to try it again, press "R"
+choice /n /c R > nul
+goto :start
